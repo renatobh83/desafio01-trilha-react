40f8f5024b6e6cdef36841c175b597190ac47c45
renatobh83 <neo_rlm@hotmail.com>
2021-09-17 06:32:19 -0300

    finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+import "../styles/sidebar.scss";
+interface sideBarProps {
+  genres: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+export function SideBar(props: sideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
